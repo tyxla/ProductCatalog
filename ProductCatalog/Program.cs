0b447755d611950ce771a72369f22830744ce00b
Marin Atanasov <tyxla@abv.bg>
2014-12-13 20:14:25 +0200

    rebuild

--- ProductCatalog/Program.cs
+++ ProductCatalog/Program.cs
@@ -7,9 +7,6 @@ namespace ProductCatalog
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
